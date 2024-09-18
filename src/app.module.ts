44a95f494064a8830ac886577e5c279a51ef55f9
danielazevedocb <daniel.santoscb@gmail.com>
2024-09-18 17:42:14 -0300

    Configurado o script para deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaService } from './database/prisma.service';
 import { CadtpgModule } from './cadtpg/cadtpg.module';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CadtpgModule],
+  imports: [CadtpgModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [],
   providers: [
     PrismaService,
